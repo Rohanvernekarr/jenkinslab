2014cef86c287660ac21df52194d642066bfb03f
rohan <rohanrv2004@gmail.com>
2025-06-10 08:05:17 +0530

    app

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int [] numbers = {1, 2, 3, 4, 5};
+        for (int number : numbers) {
+            System.out.println("Number: " + number);
+        }
+        int a=5, b=10;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
     }
 }
